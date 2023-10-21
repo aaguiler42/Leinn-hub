0f685b37c29625ba86ae2954bd0deb70469cb17a
Ilenia <ile.carboni@gmail.com>
2023-10-21 16:12:27 +0200

    Sidebar

---
+++ src/app/components/Sidebar/Links.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { FaCartPlus } from 'react-icons/fa';
+
+export const SidebarLinks= [
+  {
+    title: 'Home',
+    path: '/',
+    icon: <FaCartPlus />,
+    cName: 'nav-text'
+  },
+  {
+    title: 'Reports',
+    path: '/reports',
+    icon: <FaCartPlus />,
+    cName: 'nav-text'
+  },
+  {
+    title: 'Products',
+    path: '/products',
+    icon: <FaCartPlus />,
+    cName: 'nav-text'
+  },
+  {
+    title: 'Team',
+    path: '/team',
+    icon: <FaCartPlus />,
+    cName: 'nav-text'
+  },
+  {
+    title: 'Messages',
+    path: '/messages',
+    icon: <FaCartPlus />,
+    cName: 'nav-text'
+  },
+  {
+    title: 'Support',
+    path: '/support',
+    icon: <FaCartPlus />,
+    cName: 'nav-text'
+  }
+];
--- src/app/components/Sidebar/Sidebar.tsx
+++ src/app/components/Sidebar/Sidebar.tsx
@@ -3,6 +3,10 @@
 import { SignInButton, UserButton, useAuth } from "@clerk/nextjs";
 import React, { useState } from 'react';
 import styles from './Sidebar.module.css'
+import { SidebarLinks } from './Links';
+import { IconContext } from 'react-icons';
+import { FaBars } from 'react-icons/fa';
+import Link from 'next/link';
 
 const Sidebar = () => {
   const { isSignedIn } = useAuth();
@@ -13,30 +17,35 @@ const Sidebar = () => {
   };
 
   return (
-    <div className={styles.bg}>
-      <div className={styles.sidebar}>
-        <div className={styles.burgermenu} onClick={toggleSidebar}>
-          <div className={styles.bar}></div>
-          <div className={styles.bar}></div>
-          <div className={styles.bar}></div>
+    <div>
+      <div className='burger'>
+        <Link href='#'>
+          <FaBars onClick={setIsSidebarOpen} />
+        </Link>
+      </div>
+      <div className={styles.content}>
+        <div className={`${styles.sidebar} ${isSidebarOpen ? styles.open : ''}`}>
+          <div className={styles.logo}>
+            <Link href='#' className='closeSidebar'>
+              x
+            </Link>
+            LEINN Hub
+          </div>
+          <div>
+            <div className={styles.navMenuItems}>
+              {SidebarLinks.map((item, index) => {
+                return (
+                  <div key={index} className={styles.navText}>
+                    <Link href={item.path}>
+                      {item.icon}
+                      <span className={styles.textLinks}>{item.title}</span>
+                    </Link>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
         </div>
-        <ul>
-          <li>Item 1</li>
-          <li>Item 2</li>
-          <li>Item 3</li>
-        </ul>
-        LEINN Hub
-      { isSignedIn
-      ? <UserButton 
-          afterSignOutUrl="/"
-        />
-      : <SignInButton
-          mode="modal"
-          afterSignUpUrl="/"
-        >
-          Login
-        </SignInButton>
-      }
       </div>
     </div>
     

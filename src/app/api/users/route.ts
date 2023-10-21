e207235c2cc34a97a7d341eb5d49b1c00dfaac77
Narciso González Calderón <narsodev@gmail.com>
2023-10-21 12:19:55 +0200

    Add auth

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -1,7 +1,19 @@
 import { NextRequest, NextResponse } from "next/server";
 import prisma from "@/lib/prisma";
 
+const API_KEY = process.env.USER_CLERK_API_KEY;
+
 export async function POST(request: NextRequest) {
+  const auth = request.headers.get("API-KEY");
+  if (!API_KEY || auth !== API_KEY) {
+    return NextResponse.json(
+      {
+        message: "Unauthorized",
+      },
+      { status: 401 }
+    );
+  }
+
   const body = await request.json();
   if (!body) {
     return NextResponse.json(

6da0416bec210422e4df6394f3158a860d6954fb
Ilenia <ile.carboni@gmail.com>
2023-10-22 02:26:34 +0200

    contributions

--- src/app/components/Dashboard/Dashboard.tsx
+++ src/app/components/Dashboard/Dashboard.tsx
@@ -1,14 +1,44 @@
 import Crowdfunding from "../Crowdfunding/Crowfounding";
 import styles from "./Dashboard.module.css";
 import clsx from "clsx";
+import prisma from "@/lib/prisma";
 
-export default function Dashboard() {
+export default async function Dashboard() {
+  const contributions = await prisma.contribution.findMany({
+    include: {
+      company: {
+        select: {
+          name: true
+        },
+      }
+    },
+    take: 5,
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
   return (
-    <div className={ clsx(styles.block, styles.crowdfunding) }>
+    <div>
+      <div className={ clsx(styles.block, styles.crowdfundingBox) }>
+          <div className={styles.titleBox}>
+              <p className={styles.title}>!Ayudanos a alcanzar nuestra meta y cambiar vidas!</p>
+          </div>
+          <Crowdfunding />
+      </div>
+      <div className={ clsx(styles.block, styles.contributionsBox) }>
         <div className={styles.titleBox}>
-            <p className={styles.title}>!Ayudanos a alcanzar nuestra meta y cambiar vidas!</p>
+            <p className={styles.title}>Últimas contribuciones</p>
         </div>
-        <Crowdfunding />
+        <ul className={styles.contributions}>
+        {contributions.map((contribution) => {
+          return (
+            <li key={contribution.id}>
+              <span>{contribution.company.name} {contribution.amount}</span>
+            </li>
+          );
+        })}
+      </ul>
+      </div>
     </div>
   );
 }

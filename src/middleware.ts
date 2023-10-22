71da2ab744c08dcd05aacc40814d1919471d8606
aaguiler <aguileragarciadev@gmail.com>
2023-10-22 03:33:41 +0200

    Fix middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,6 +2,7 @@ import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
   publicRoutes: ["/", "/users", "/users/:id", "/onboarding"],
+  ignoredRoutes: ["/((?!api|trpc))(_next.*|.+\.[\w]+$)"]
 });
 
 export const config = {
